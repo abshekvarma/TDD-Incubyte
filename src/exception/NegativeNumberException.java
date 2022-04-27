1deddb53481579d6c05e848920c9e2da65f27920
abhishek <abhishek.varma.atwork@gmail.com>
2022-04-27 12:11:04 +0530

    Commit 3

---
+++ src/exception/NegativeNumberException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class NegativeNumberException extends Exception {
+
+    public NegativeNumberException(String errorMessage) {
+        super(errorMessage);
+    }
+}
--- src/service/AdditionImpl.java
+++ src/service/AdditionImpl.java
@@ -1,15 +1,34 @@
 package service;
 
+import exception.NegativeNumberException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdditionImpl implements IAddition {
     @Override
     public int sum(String numbers) {
-        String[] integers = numbers.split("[,\\n]");
+        String[] integers = numbers.split("[//]*[,;\n]");
+        List<Integer> negativeNums = new ArrayList<>();
         int sum = 0;
         if (integers.length == 1) {
             return 0;
         }
         for (int i = 0; i < integers.length; i++)
-            sum += Integer.parseInt(integers[i]);
+            try {
+                int val = Integer.parseInt(integers[i]);
+                if(val<0){
+                    negativeNums.add(val);
+                    throw new NegativeNumberException("negatives not allowed");
+                }
+                sum+=val;
+            }catch(NegativeNumberException e){
+                sum=0;
+                if(i+1==integers.length){
+                    System.out.println(e.getMessage()+", Numbers are "+negativeNums);}
+            } catch (NumberFormatException e) {
+                continue;
+            }
         return sum;
     }
 }
